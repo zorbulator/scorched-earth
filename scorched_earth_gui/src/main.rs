e71fff36c3abce250e7e8b92eaeef2dd355c2739
Zorb Ulator <gpg@zorbulator.com>
2023-04-23 17:00:53 -0700

    add web support

--- scorched_earth_gui/src/main.rs
+++ scorched_earth_gui/src/main.rs
@@ -1,10 +1,31 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use eframe::egui;
 use scorched_earth_gui_lib::State;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(320.0, 240.0)),
         ..Default::default()
     };
-    eframe::run_native("Scorched Earth", options, Box::new(|_| Box::new(State::default())))
+    eframe::run_native(
+        "Scorched Earth",
+        options,
+        Box::new(|_| Box::new(State::default())),
+    )
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    console_error_panic_hook::set_once();
+    let web_options = eframe::WebOptions::default();
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::start_web(
+            "canvas",
+            web_options,
+            Box::new(|_| Box::new(State::default())),
+        )
+        .await
+        .expect("failed to start eframe");
+    })
 }
