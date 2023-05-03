d6b5a973931c647216b8dc636bda5fabf44a4fb3
Deven Tseng <dtseng@ucsb.edu>
2023-05-03 16:31:55 -0700

    Finish input.rs v2

--- scorched_earth_gui/src/screens/input.rs
+++ scorched_earth_gui/src/screens/input.rs
@@ -15,68 +15,86 @@ pub fn render(screen: &mut Screen, ui: &mut egui::Ui) {
                 .font(FontId::proportional(25.0))
                 .size(30.0)
             );
-            ui.add_space(10.0);
+            ui.add_space(50.0);
             ui.add(egui::widgets::text_edit::TextEdit::singleline(joinid)
                .min_size(Vec2 { x: 300.0, y: 30.0 })
                .font(FontId::proportional(25.0))
             );
 
-            ui.add_space(20.0);
-            let mut grid = egui::Grid::new("keypad");
-            egui::Grid::new("keypad").show(ui, |ui| {
-                for _i in '0'..='9' {
-                    let keypad_button = egui::widgets::Button::new(RichText::new(_i.to_string())
-                                                                   .size(10.0)
-                                                                   .color(Color32::WHITE))
-                        .min_size(Vec2 { x: 100.0, y: 50.0 });
-                    if ui.add(keypad_button).clicked() {
-                        joinid.push(_i);
-                    }
-                    if _i == '2' {
-                        ui.end_row();
-                    }
-                    if _i == '5' {
-                        ui.end_row();
-                    }
-                    if _i == '8' {
-                        ui.end_row();
-                    }
+            ui.add_space(250.0);
+        });
+
+        let clear_button = egui::widgets::Button::new(RichText::new("clear")
+            .size(30.0)
+            .color(Color32::WHITE))
+            .min_size(Vec2 { x: 100.0, y: 50.0 });
+
+        let back_button = egui::widgets::Button::new(RichText::new("back")
+            .size(30.0)
+            .color(Color32::WHITE))
+            .min_size(Vec2 { x: 100.0, y: 50.0 });
+
+        let join_button = egui::widgets::Button::new(RichText::new("join")
+            .size(30.0)
+            .color(Color32::WHITE))
+            .min_size(Vec2 { x: 100.0, y: 50.0 });
+
+        egui::Grid::new("keypad")
+            .show(ui, |ui| {
+            ui.add_space(40.0);
+            let num_array: [char; 9] = ['7', '8', '9', '4', '5', '6', '1', '2', '3'];
+            for _i in num_array{
+                let keypad_button = egui::widgets::Button::new(RichText::new(_i.to_string())
+                    .size(30.0)
+                    .color(Color32::WHITE))
+                    .min_size(Vec2 { x: 100.0, y: 50.0 });
+                if ui.add(keypad_button).clicked() {
+                    joinid.push(_i);
                 }
+                if _i == '9' {
+                    ui.end_row();
+                    ui.add_space(40.0);
+                }
+                if _i == '6' {
+                    ui.end_row();
+                    ui.add_space(40.0);
+                }
+                if _i == '3' {
+                    ui.end_row();
+                    ui.add_space(40.0);
+                }
+            }
+
+            if ui.add(clear_button).clicked() {
+                joinid.clear();
+            }
+
+            let zero_button = egui::widgets::Button::new(RichText::new('0')
+                .size(30.0)
+                .color(Color32::WHITE))
+                .min_size(Vec2 { x: 100.0, y: 50.0 });
+            if ui.add(zero_button).clicked() {
+                joinid.push('0');
+            }
+
+            if ui.add(back_button).clicked() {
+                joinid.pop();
+            }
+            ui.end_row();
+        });
+
+        ui.vertical_centered(|ui| {
+            if ui.add(join_button).clicked() {
                 // only set if join is clicked
                 let mut join_rx: Option<Receiver<_>> = None;
-
-                let clear_button = egui::widgets::Button::new(RichText::new("clear")
-                                                              .size(10.0)
-                                                              .color(Color32::WHITE))
-                    .min_size(Vec2 { x: 100.0, y: 50.0 });
-
-                let back_button = egui::widgets::Button::new(RichText::new("back")
-                                                             .size(10.0)
-                                                             .color(Color32::WHITE))
-                    .min_size(Vec2 { x: 100.0, y: 50.0 });
-
-                let join_button = egui::widgets::Button::new(RichText::new("join")
-                                                             .size(10.0)
-                                                             .color(Color32::WHITE))
-                    .min_size(Vec2 { x: 100.0, y: 50.0 });
-
-                if ui.add(clear_button).clicked() {
-                    joinid.clear();
-                }
-                if ui.add(back_button).clicked() {
-                    joinid.pop();
-                }
-                ui.end_row();
-                if ui.add(join_button).clicked() {
-                    let (tx, rx) = channel();
-                    let joinid2 = joinid.clone();
-                    thread::spawn(move || {
-                        tx.send(Connection::conn(ADDR, joinid2.as_bytes())).unwrap();
-                    });
-                    // *screen = Screen::Join(rx);
-                    join_rx = Some(rx);
-                }
-            });
+                let (tx, rx) = channel();
+                let joinid2 = joinid.clone();
+                thread::spawn(move || {
+                    tx.send(Connection::conn(ADDR, joinid2.as_bytes())).unwrap();
+                });
+                // *screen = Screen::Join(rx);
+                join_rx = Some(rx);
+            }
         });
     }
 }
