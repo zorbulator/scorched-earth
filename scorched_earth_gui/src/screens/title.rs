650ce694fc4bd98ec50a0bcd24c12c194db51c73
Deven Tseng <dtseng@ucsb.edu>
2023-05-02 20:47:04 -0700

    Finish title page v2

--- scorched_earth_gui/src/screens/title.rs
+++ scorched_earth_gui/src/screens/title.rs
@@ -3,30 +3,44 @@ use eframe::{egui::{self, RichText, FontId}, epaint::{Color32, Vec2}};
 
 pub fn render(state: &mut State, ui: &mut egui::Ui) {
 
-    let host_button = egui::widgets::Button::new(RichText::new("Host Game").color(Color32::WHITE)).min_size(Vec2 { x: 100.0, y: 50.0 });
-    let join_button = egui::widgets::Button::new(RichText::new("Join Game").color(Color32::WHITE)).min_size(Vec2 { x: 100.0, y: 50.0 });
-    let rules_button = egui::widgets::Button::new(RichText::new("Rules").color(Color32::WHITE)).min_size(Vec2 { x: 100.0, y: 50.0 });
+    let host_button = egui::widgets::Button::new(RichText::new("Host Game")
+        .size(30.0)
+        .color(Color32::WHITE))
+        .min_size(Vec2 { x: 300.0, y: 50.0 });
+    let join_button = egui::widgets::Button::new(RichText::new("Join Game")
+        .size(30.0)
+        .color(Color32::WHITE))
+        .min_size(Vec2 { x: 300.0, y: 50.0 });
+    let rules_button = egui::widgets::Button::new(RichText::new("Rules")
+        .size(30.0)
+        .color(Color32::WHITE))
+        .min_size(Vec2 { x: 300.0, y: 50.0 });
     
     let svg_image = egui_extras::RetainedImage::from_svg_bytes_with_size(
-        "fire-zone.svg",
-        include_bytes!("../assets/fire-zone.svg"),
+        "flame-colored.svg",
+        include_bytes!("../assets/flame-colored.svg"),
         egui_extras::image::FitTo::Original,
         )
     .unwrap();
     ui.vertical_centered(|ui| {
-        ui.add_space(20.0);
-        ui.heading(RichText::new("Scorched Earth").color(Color32::DARK_RED).font(FontId::proportional(40.0)));
+        ui.add_space(30.0);
+        ui.heading(RichText::new("Scorched Earth")
+            .color(Color32::WHITE)
+            // .color(Color32::from_rgb(173, 39, 12))
+            .font(FontId::proportional(40.0))
+            .size(50.0)
+        );
         ui.add_space(10.0);
         svg_image.show_size(ui, Vec2 { x: 300.0, y: 300.0 });
-        ui.add_space(30.0);
+        ui.add_space(50.0);
         if ui.add(host_button).clicked() {
             state.screen = Screen::Host;
         }
-        ui.add_space(20.0);
+        ui.add_space(30.0);
         if ui.add(join_button).clicked() {
             state.screen = Screen::Rules;
         }
-        ui.add_space(20.0);
+        ui.add_space(30.0);
         if ui.add(rules_button).clicked() {
             state.screen = Screen::Join;
         }
