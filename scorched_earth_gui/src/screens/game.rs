8217b486fb0a489dc3ece9235fd8469d097387e4
Zorb Ulator <gpg@zorbulator.com>
2023-05-03 23:02:31 -0700

    fix end screen and formatting

--- scorched_earth_gui/src/screens/game.rs
+++ scorched_earth_gui/src/screens/game.rs
@@ -1,6 +1,6 @@
 use std::{sync::mpsc::channel, thread};
 
-use crate::{Screen, convert_color};
+use crate::{convert_color, Screen};
 use eframe::{
     egui::{self, RichText},
     epaint::{Color32, Rect, Rounding, Vec2},
@@ -36,7 +36,7 @@ fn draw_board(ui: &mut egui::Ui, board: &Board, preview_move: &Option<Move>, i: 
                         Rect {
                             min: corner,
                             max: corner + egui::vec2(w, w),
-        },
+                        },
                         Rounding::none(),
                         color,
                     );
@@ -134,83 +134,97 @@ pub fn render(screen: &mut Screen, ui: &mut egui::Ui) {
         } else {
             let mut input: Option<Direction> = None;
 
-            let left_button = egui::widgets::Button::new(RichText::new("left")
-                .size(20.0)
-                .color(Color32::WHITE));
-                
-            let right_button = egui::widgets::Button::new(RichText::new("right")
-                .size(20.0)
-                .color(Color32::WHITE));
-                
-            let down_button = egui::widgets::Button::new(RichText::new("down")
-                .size(20.0)
-                .color(Color32::WHITE));
-                
-            let up_button = egui::widgets::Button::new(RichText::new("up")
-                .size(20.0)
-                .color(Color32::WHITE));
-                
-            let done_button = egui::widgets::Button::new(RichText::new("done")
-                .size(20.0)
-                .color(Color32::WHITE));
-                
+            let left_button =
+                egui::widgets::Button::new(RichText::new("left").size(20.0).color(Color32::WHITE));
+
+            let right_button =
+                egui::widgets::Button::new(RichText::new("right").size(20.0).color(Color32::WHITE));
+
+            let down_button =
+                egui::widgets::Button::new(RichText::new("down").size(20.0).color(Color32::WHITE));
+
+            let up_button =
+                egui::widgets::Button::new(RichText::new("up").size(20.0).color(Color32::WHITE));
+
+            let done_button =
+                egui::widgets::Button::new(RichText::new("done").size(20.0).color(Color32::WHITE));
+
             let row_length = ui.available_width() * 0.95;
             ui.vertical_centered(|ui| {
-                ui.allocate_ui(Vec2 { x: row_length, y: 50.0}, |ui| {
-
-                    ui.columns(3, |columns| {
-                        columns[1].vertical_centered(|ui| {
-                            if ui.add_sized(ui.available_size(), up_button).clicked() {
-                                input = Some(Direction::Up);
-                            }
-                        })
-                    });
-                });      
-
-                ui.allocate_ui(Vec2 { x: row_length, y: 50.0}, |ui| {
-                    ui.columns(3, |columns| {
-                        columns[0].vertical_centered(|ui| {
-                            if ui.add_sized(ui.available_size(), left_button).clicked() {
-                                input = Some(Direction::Left);
-                            }
+                ui.allocate_ui(
+                    Vec2 {
+                        x: row_length,
+                        y: 50.0,
+                    },
+                    |ui| {
+                        ui.columns(3, |columns| {
+                            columns[1].vertical_centered(|ui| {
+                                if ui.add_sized(ui.available_size(), up_button).clicked() {
+                                    input = Some(Direction::Up);
+                                }
+                            })
                         });
-                        columns[1].vertical_centered(|ui| {
-                            if ui.add_sized(ui.available_size(), done_button).clicked() {
-                                if let Some(m) = preview_move {
-                                    if board.is_move_valid(i, *m) {
-                                        let res = board.make_move(i, *m);
-                                        if let Some(color) = res.winner {
-                                            error_message = Some(format!("{:?} won!", color));
+                    },
+                );
+
+                ui.allocate_ui(
+                    Vec2 {
+                        x: row_length,
+                        y: 50.0,
+                    },
+                    |ui| {
+                        ui.columns(3, |columns| {
+                            columns[0].vertical_centered(|ui| {
+                                if ui.add_sized(ui.available_size(), left_button).clicked() {
+                                    input = Some(Direction::Left);
+                                }
+                            });
+                            columns[1].vertical_centered(|ui| {
+                                if ui.add_sized(ui.available_size(), done_button).clicked() {
+                                    if let Some(m) = preview_move {
+                                        if board.is_move_valid(i, *m) {
+                                            let res = board.make_move(i, *m);
+                                            if let Some(color) = res.winner {
+                                                let lost =
+                                                    color == board.players[conn_player].color;
+                                                won = Some((!lost, color));
+                                            }
+                                            match conn.lock().unwrap().send_move(MoveMessage {
+                                                new_board: board.clone(),
+                                                new_move: *m,
+                                                player: i,
+                                            }) {
+                                                Err(e) => error_message = Some(e.to_string()),
+                                                _ => {}
+                                            }
+                                            *preview_move = None;
                                         }
-                                        match conn.lock().unwrap().send_move(MoveMessage {
-                                            new_board: board.clone(),
-                                            new_move: *m,
-                                            player: i,
-                                        }) {
-                                            Err(e) => error_message = Some(e.to_string()),
-                                            _ => {}
-                                        }
-                                        *preview_move = None;
                                     }
                                 }
-                            }
+                            });
+                            columns[2].vertical_centered(|ui| {
+                                if ui.add_sized(ui.available_size(), right_button).clicked() {
+                                    input = Some(Direction::Right);
+                                }
+                            });
                         });
-                        columns[2].vertical_centered(|ui| {
-                            if ui.add_sized(ui.available_size(), right_button).clicked() {
-                                input = Some(Direction::Right);
-                            }
+                    },
+                );
+                ui.allocate_ui(
+                    Vec2 {
+                        x: row_length,
+                        y: 50.0,
+                    },
+                    |ui| {
+                        ui.columns(3, |columns| {
+                            columns[1].vertical_centered(|ui| {
+                                if ui.add_sized(ui.available_size(), down_button).clicked() {
+                                    input = Some(Direction::Down);
+                                }
+                            })
                         });
-                    });
-                }); 
-                ui.allocate_ui(Vec2 { x: row_length, y: 50.0}, |ui| {
-                    ui.columns(3, |columns| {
-                        columns[1].vertical_centered(|ui| {
-                            if ui.add_sized(ui.available_size(), down_button).clicked() {
-                                input = Some(Direction::Down);
-                            }
-                        })
-                    });
-                });      
+                    },
+                );
             });
             if let Some(dir) = input {
                 match preview_move {
